af2af48ba63d412340a54b9e85ad16d831d28ecc
Josef Lekardal <josef@workbuster.se>
2020-05-25 01:03:07 +0200

    serve script from functions

--- functions/test.js
+++ functions/test.js
@@ -2,9 +2,66 @@ exports.handler = async (event) => {
   console.log('This is a get method', event)
   return {
     statusCode: 200,
-    body: 'HELLLOOOO',
+    body: `$(document).ready(function () {
+    var $form = $('.hillsong-form')
+    var $submitBtn = $form.find('input[type=submit]');
+    var $requiredFields = $form.find(':required').each(function () {
+      $('<span style="color: red">*</span>').insertBefore($(this).prev('br'));
+    });
+    $form.on('submit', function (event) {
+      event.preventDefault();
+      $submitBtn.prop('disabled', true);
+      var rowData = $(this)
+        .serializeArray()
+        .reduce(function (acc, cur) {
+            acc[cur.name] = cur.value
+            return acc
+          }, {});
+      var sendingText = 'Skickar ';
+      var intervalId = setInterval(function () {
+          $submitBtn.val(
+            sendingText = sendingText.length < 11
+              ? sendingText + '.'
+              : 'Skickar '
+          );
+      }, 500)
+
+      $.ajax({
+        url: 'https://sheets-forms.netlify.app/.netlify/functions/save-to-sheet',
+        method: 'POST',
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        crossDomain: true,
+        data: JSON.stringify({
+          sheetId: $(this).attr('id'),
+          sheetName: $(this).attr('name'),
+          rowData: rowData
+        }),
+        success: function(response) {
+          $submitBtn.prop('disabled', false);
+          clearInterval(intervalId);
+          $form.trigger('reset');
+          $submitBtn.val('Skickat!');
+          setTimeout(function() {
+            $form
+              .find('input[type=submit]')
+              .val('Skicka');
+            }, 3000);
+        },
+        error: function (error) {
+          $submitBtn
+            .prop('disabled', false)
+            .val('Skicka');
+          console.error('Error posting form data:');
+          console.log(error);
+        }
+      });
+    });
+  });
+    `,
     headers: {
-      'Access-Control-Allow-Origin': '*'
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'text/javascript; charset=UTF-8'
     }
   }
 }
